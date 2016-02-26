ec62868a87ddcc2b97ce0124e58927e3cf0e4712
Ismael Moral <imoral@nohacefaltapapel-et.net>
2016-02-26 12:52:39 +0100

    - Eliminados ejemplos, y cargada nueva estructura

--- src/test.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @author JasterTDC <jastertdc@gmail.com>
- **/
-(function(){
-  Jima = function(container){
-    this._container = document.getElementById(container);
-  };
-})();
--- src/test_2.js
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @author  Ismael Moral <imoral@nohacefaltapapel-et.net>
- **/
-(function(){
-
-})();
